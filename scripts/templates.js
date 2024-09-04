89b7b4f4d77dd7fd69611cd8e3ddd776c019d673
RicardoP-19 <ricardo-pinto@web.>
2024-09-04 23:22:41 +0200

    animation

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,21 +1,21 @@
 function returnHTMLPokemon(index) {
   return /*html*/`
-      <div class="card-animation">
+      <div class="card-animation bga_${allPokemons[index].types[0]}">
         <div class="card">
-          <div class="card-content">
+          <div class="card-content bgi_${allPokemons[index].types[0]}">
             <div class="card-head">
-              <h3>${allPokemons[index].name}</h3>
+              <h2>${allPokemons[index].name}</h2>
               <span class="head-right">
-                ${allPokemons[index].types}
+                ${allPokemons[index].types.join(' ')}
               </span>
             </div>
             <div class="card-image">
               <img src="${allPokemons[index].image}"/>
             </div>
             <div class="card-infos">
-              <div class="card-typ">
+              <!-- <div class="card-typ">
                 <span>Types</span>
-              </div>
+              </div> -->
               <!-- ${allPokemons[index].moves} -->
               <!-- ${allPokemons[index].specis} -->
             </div>
